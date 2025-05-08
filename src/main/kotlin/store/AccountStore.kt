fce072b7c1db83548ccbf5b61e31ed9e3e91ad70
Agualuza <iagoagualuza@msn.com>
2025-05-08 01:12:45 -0300

    fix error on first

--- src/main/kotlin/store/AccountStore.kt
+++ src/main/kotlin/store/AccountStore.kt
@@ -7,7 +7,7 @@ object AccountStore {
     val accounts = CopyOnWriteArrayList<Account>()
 
     fun getAccountById(id: Int) =
-        this.accounts.first { it.id == id }
+        this.accounts.firstOrNull { it.id == id }
 
     fun deleteAccountById(id: Int) =
         this.accounts.removeIf { it.id == id }
